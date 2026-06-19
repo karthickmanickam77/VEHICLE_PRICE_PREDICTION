5988db2ce3bfeee6df75e53715da53d55e9289c1
Karthick M <karthickmanickam77@gmail.com>
2026-06-19 12:56:06 +0200

    Endpoint Initial Finish

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,11 @@
+import os
 import uvicorn
 
 if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8000))
+
     uvicorn.run(
         "app.app:app",
         host="0.0.0.0",
-        port=8000,
+        port=port,
     )
